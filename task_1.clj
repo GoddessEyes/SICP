40b8d55046dee63ea9cd893943474699b61132a6
igor shcherbakov <igor.shcherbakov@fogstream.ru>
2020-06-29 21:55:59 +1000

    From start to 1.1.6

---
+++ task_1.clj
@@ -0,0 +1,16 @@
+; (/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5))))) (* 3 (- 6 2) (- 2 7)))
+
+
+
+(defn squareInMax [a b c] 
+    (cond
+        (and (> a c) (> b c)) (+ (* b b) (* a a))
+        (and (> a b) (> c b)) (+ (* c c) (* a a))
+        (and (> c a) (> b a)) (+ (* b b) (* c c))
+    ))
+
+(squareInMax 3 2 5)
+
+
+
+(Math/pow 3 2)
